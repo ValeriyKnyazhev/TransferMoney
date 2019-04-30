83c7c2049482ac507fcfbbc605567c4a8a0a0da9
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2019-04-30 12:01:11 +0300

    init commit

---
+++ src/main/java/valeriy/knyazhev/transfermoney/Main.java
@@ -0,0 +1,12 @@
+package valeriy.knyazhev.transfermoney;
+
+/**
+ * @author Valeriy Knyazhev
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello!");
+    }
+
+}
