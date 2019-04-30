a1c8b3741f4456e192442fb42ae6b9ccbdc473c1
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2019-04-30 12:17:40 +0300

    add actions with money for account

--- src/main/java/valeriy/knyazhev/transfermoney/domain/model/Account.java
+++ src/main/java/valeriy/knyazhev/transfermoney/domain/model/Account.java
@@ -14,6 +14,25 @@ public class Account {
         this.amount = 0;
     }
 
+    private static void checkAmountCorrectness(long amount) {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount money must be positive.");
+        }
+    }
+
+    public void putMoney(long amount) {
+        checkAmountCorrectness(amount);
+        this.amount += amount;
+    }
+
+    public void takeMoney(long amount) {
+        checkAmountCorrectness(amount);
+        if (amount > this.amount) {
+            throw new IllegalStateException("Account " + this.id + " has not enough money for operation.");
+        }
+        this.amount -= amount;
+    }
+
     public int id() {
         return this.id;
     }
