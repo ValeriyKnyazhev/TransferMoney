8258e7c3df50db11420191190d2b27a609574d67
Valeriy Knyazhev <valeriy.knyazhev@yandex.ru>
2019-04-30 12:53:02 +0300

    add account manager for main operations (find, create, put, take and transfer money)

---
+++ src/main/java/valeriy/knyazhev/transfermoney/application/AccountData.java
@@ -0,0 +1,25 @@
+package valeriy.knyazhev.transfermoney.application;
+
+
+/**
+ * @author Valeriy Knyazhev
+ */
+public class AccountData {
+
+    private final int id;
+
+    private final long amount;
+
+    public AccountData(int id, long amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public int id() {
+        return this.id;
+    }
+
+    public long amount() {
+        return this.amount;
+    }
+}
---
+++ src/main/java/valeriy/knyazhev/transfermoney/application/AccountManager.java
@@ -0,0 +1,76 @@
+package valeriy.knyazhev.transfermoney.application;
+
+import valeriy.knyazhev.transfermoney.domain.model.Account;
+import valeriy.knyazhev.transfermoney.domain.model.AccountRepository;
+
+/**
+ * @author Valeriy Knyazhev
+ */
+public class AccountManager {
+
+    private static final AccountManager INSTANCE = new AccountManager();
+
+    private final AccountRepository repository;
+
+    private AccountManager() {
+        this.repository = new InMemoryAccountRepository();
+    }
+
+    public static AccountManager getInstance() {
+        return INSTANCE;
+    }
+
+    private static AccountData constructData(Account account) {
+        return new AccountData(account.id(), account.amount());
+    }
+
+    public AccountData createNewAccount() {
+        Account newAccount = this.repository.createNewAccount();
+        return constructData(newAccount);
+    }
+
+    public void putMoney(int accountId, long amount) {
+        Account account = this.repository.findById(accountId);
+        synchronized (account) {
+            account.putMoney(amount);
+        }
+    }
+
+    public void takeMoney(int accountId, long amount) {
+        Account account = this.repository.findById(accountId);
+        synchronized (account) {
+            account.takeMoney(amount);
+        }
+    }
+
+    public AccountData findById(int accountId) {
+        Account account = this.repository.findById(accountId);
+        return constructData(account);
+    }
+
+    public void transferMoney(int fromAccountId, int toAccountId, long amount) {
+        // TODO check if account equals
+        Account accountFrom = this.repository.findById(fromAccountId);
+        Account accountTo = this.repository.findById(toAccountId);
+
+        if (accountFrom.id() < accountTo.id()) {
+            synchronized (accountFrom) {
+                synchronized (accountTo) {
+                    doTransferMoney(accountFrom, accountTo, amount);
+                }
+            }
+        } else {
+            synchronized (accountTo) {
+                synchronized (accountFrom) {
+                    doTransferMoney(accountFrom, accountTo, amount);
+                }
+            }
+        }
+    }
+
+    private void doTransferMoney(Account accountFrom, Account accountTo, long amount) {
+        accountFrom.takeMoney(amount);
+        accountTo.putMoney(amount);
+    }
+
+}
